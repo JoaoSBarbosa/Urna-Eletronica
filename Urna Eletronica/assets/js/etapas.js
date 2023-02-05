b62e3da453822308fc897902752cef9a5a5fb57e
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-05 12:34:36 -0300

    Javascript - Criação de variaveis de manipulação do DOM

---
+++ Urna Eletronica/assets/js/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 13042,
+        nome: "Faulano do 13",
+        partido: "ESTRELA",
+        fotos: [
+          { url: "13042-vereador.jpg", legenda: "Vereador", small: false },
+        ],
+      },
+      {
+        numero: 21104,
+        nome: "Beltrano do 21",
+        partido: "CAPITÃO PATRIA",
+        fotos: [{ url: "21104-vereador.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 13,
+        nome: "Ciclano",
+        partido: "ESTRELA",
+        vice: "Ciclano vice",
+        fotos: [
+          { url: "13-candidato.jpg", legenda: "Prefeito" },
+          {
+            url: "13-2-Vice-candidato.jpg",
+            legenda: "Vice-Prefeito",
+            small: true,
+          },
+        ],
+      },
+      {
+        numero: 21,
+        nome: "Zulano",
+        partido: "CAPITÃO PATRIA",
+        vice: "Zul",
+        fotos: [
+          { url: "21-candidato.jpg", legenda: "Prefeito" },
+          { url: "21-2-candidato.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
---
+++ Urna Eletronica/assets/js/script.js
@@ -0,0 +1,8 @@
+// Variaveis - manipular o DOM
+let seuVotoPara = document.querySelector('.d-1-card1 span');
+let cargo = document.querySelector('.d-1-card2 span');
+let descricao = document.querySelector('.d-1-card4');
+let avisos = document.querySelector('.d-2');
+
+let lateralCard = document.querySelector('.d-1-right');
+let squadNumeros = document.querySelector('.d-1-card3');
